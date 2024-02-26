2a6af4b06797f5e95394ca498514c7a0e33be866
wessam18 <wessamabdelnabi903@gmail.com>
2024-02-26 04:48:00 +0200

    edit

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,13 @@
-#  using Puppet to make changes to our configuration file
+# using puppet to configure the system
 
-file_line { '/etc/ssh/sshd_config':
-    ensure => present,
-    path   => '/etc/ssh/sshd_config',
-    line   => 'PasswordAuthentication no',
-    match  => '^PasswordAuthentication=',
-}
-
-file_line { '~/.ssh/scool':
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => 'IdentityFile ~/.ssh/school',
-    match  => '^IdentityFile=',
+file_line {
+  'passAuth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  ;
+  'keyLocation':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
