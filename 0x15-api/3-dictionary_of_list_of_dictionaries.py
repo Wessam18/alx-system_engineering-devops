dd608ff4fbe01a0f4f6e98d22f790f450be5680a
wessam18 <wessamabdelnabi903@gmail.com>
2024-04-20 08:33:34 +0200

    3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,8 +13,6 @@ if __name__ == "__main__":
     tasks = requests.get(
             "https://jsonplaceholder.typicode.com/todos/").json()
 
-    all_employee = {}
-
     for user in users:
         all_tasks = []
         for task in tasks:
@@ -25,7 +23,7 @@ if __name__ == "__main__":
                     "completed": task.get("completed")
                 }
                 all_tasks.append(user_task)
-        all_employee[user.get("id")] = all_tasks
+        all_employee = {user.get("id"): all_tasks}
 
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump(all_employee, jsonfile)
